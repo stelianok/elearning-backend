0d19264e88b1d19742db0d8df5ebddf51019f288
Kauã Steliano <stelianok@gmail.com>
2021-02-17 10:50:02 -0300

    Rotas para autenticação desenvolvidas.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import usersRouter from './users.routes';
+import sessionsRouter from './session.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
