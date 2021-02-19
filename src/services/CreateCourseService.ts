0b9fe3623923718fe8a6396e7936467a5170f445
Kauã Steliano <stelianok@gmail.com>
2021-02-19 13:09:34 -0300

    Service para criação de cursos desenvolvido.

---
+++ src/services/CreateCourseService.ts
@@ -0,0 +1,33 @@
+import { getRepository } from 'typeorm';
+
+import Course from '../models/Course';
+import AppError from '../errors/AppError';
+
+interface RequestDTO {
+  name: string;
+  image?: string | undefined;
+}
+
+export default class CreateCourseService {
+
+  public async execute({ name, image }: RequestDTO): Promise<Course> {
+    const courseRepository = getRepository(Course);
+    const defaultImage = 'https://github.com/stelianok/elearningApp/blob/main/src/assets/img/Talk.png';
+
+    const courseExists = await courseRepository.findOne({ where: { name } });
+
+    if (courseExists) {
+      throw new AppError("A course with the same name is already registered");
+    }
+
+    if (!image) {
+      image = defaultImage;
+    }
+    console.log(image);
+    const course = courseRepository.create({ name, image });
+
+    await courseRepository.save(course);
+
+    return course;
+  }
+}
