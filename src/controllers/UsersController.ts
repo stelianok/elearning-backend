05433261304e487a381c422bed979648b6b21c70
Kauã Steliano <stelianok@gmail.com>
2021-02-27 08:59:40 -0300

    Implemented input validation.

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
+
 import CreateUserService from '../services/CreateUserService';
 import ListUserService from '../services/ListUsersService';
 
@@ -12,14 +14,26 @@ export default class UsersController {
   }
   async create(request: Request, response: Response) {
     const { name, email, password } = request.body;
-
     const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+      password: yup.string().required(),
+    });
 
-    // @ts-expect-error
-    delete user.password;
+    try {
+      await schema.validate(request.body, { abortEarly: false });
 
-    return response.json(user);
+      const user = await createUser.execute({ name, email, password });
+
+      // @ts-expect-error
+      delete user.password;
+
+      return response.json(user);
+    }
+    catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
