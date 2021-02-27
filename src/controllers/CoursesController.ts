a2537ef93dd026aa56236cbf581e2c0713c1a364
Kauã Steliano <stelianok@gmail.com>
2021-02-27 10:02:33 -0300

    Implemented input validation on all methods.

--- src/controllers/CoursesController.ts
+++ src/controllers/CoursesController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 import CreateCourseService from '../services/CreateCourseService';
 import DeleteCourseService from '../services/DeleteCourseService';
 import ListCoursesService from '../services/ListCoursesService';
@@ -17,31 +18,65 @@ export default class CoursesController {
 
   async create(request: Request, response: Response) {
     const { name, image } = request.body;
-
     const createCourseService = new CreateCourseService();
-    const course = await createCourseService.execute({ name, image });
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      image: yup.string().url(),
+    });
 
-    return response.json(course);
+    try {
+      await schema.validate(request.body);
+
+      const course = await createCourseService.execute({ name, image });
+
+      return response.json(course);
+    }
+    catch (error) {
+      return response.status(422).json(error);
+    }
   }
 
   async update(request: Request, response: Response) {
     const { id } = request.params;
     const { name, image } = request.body;
-
     const updateCourseService = new UpdateCourseService();
 
-    const updatedCourse = await updateCourseService.execute({ id, name, image });
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      image: yup.string().url(),
+    });
 
-    return response.json(updatedCourse);
+    try {
+      await Promise.all([
+        yup.string().uuid().required().validate(id),
+        schema.validate(request.body)
+      ]);
+
+      const updatedCourse = await updateCourseService.execute({ id, name, image });
+
+      return response.json(updatedCourse);
+    }
+    catch (error) {
+      return response.status(422).json(error);
+    }
+
+
+
   }
 
   async delete(request: Request, response: Response) {
     const { id } = request.params;
-
     const deleteCourseService = new DeleteCourseService();
+    try {
+      await yup.string().uuid().required().validate(id);
 
-    await deleteCourseService.execute(id);
+      await deleteCourseService.execute(id);
 
-    return response.sendStatus(204);
+      return response.sendStatus(204);
+
+    }
+    catch (error) {
+      return response.status(422).json(error);
+    }
   }
 }
