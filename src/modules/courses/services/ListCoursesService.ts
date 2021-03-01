49d5166a9321b3a74bc89c079a2c0f9f888c821c
Kauã Steliano <stelianok@gmail.com>
2021-03-01 15:02:04 -0300

    Now listCourse returns number of lessons.

--- src/modules/courses/services/ListCoursesService.ts
+++ src/modules/courses/services/ListCoursesService.ts
@@ -1,20 +1,38 @@
 import { getRepository, Like } from 'typeorm';
 import Course from '../infra/typeorm/entities/Course';
+import Lesson from '../infra/typeorm/entities/Lesson';
 
+
+interface CourseWithNumberOfLessons extends Course {
+  numberOfLessons: number;
+}
 export default class ListCoursesService {
-  public async execute(name?: string): Promise<Course[]> {
+  public async execute(name?: string): Promise<CourseWithNumberOfLessons[]> {
     const coursesRepository = getRepository(Course);
+    const lessonsRepository = getRepository(Lesson);
 
-    if (!name) {
-      const courses = await coursesRepository.find();
-      return courses;
+    let courses = await coursesRepository.find();
+    const lessons = await lessonsRepository.find();
+
+    if (name) {
+      courses = await coursesRepository.find({
+        where: {
+          name: Like(`%${name}%`),
+        },
+      });
     }
-    const courses = await coursesRepository.find({
-      where: {
-        name: Like(`%${name}%`)
-      }
-    });
 
-    return courses;
+    const coursesWithNumberOfLessons = courses.map((course) => {
+
+      const NumberOfLesson = lessons.filter((lesson) => lesson.course_id === course.id).length;
+      return (
+        {
+          ...course,
+          numberOfLessons: NumberOfLesson
+        }
+      )
+    })
+    return coursesWithNumberOfLessons;
+
   }
 }
